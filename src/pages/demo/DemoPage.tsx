7730e9922eaebf2e7d13befbe7184256745be3be
Quang Truong <quang.truongdinh@nashtechglobal.com>
2024-09-19 15:45:16 +0700

    Update Demo Function Component

--- src/pages/demo/DemoPage.tsx
+++ src/pages/demo/DemoPage.tsx
@@ -24,7 +24,7 @@ const DemoPage = () => {
             {/* Demo css */}
             <DemoCSSComponent />
             {/* Demo routing */}
-            <DemoRoutingWithNavlink />
+            {/* <DemoRoutingWithNavlink /> */}
         </>
     );
 }
--- src/pages/demo/components/DemoFunctionComponent.tsx
+++ src/pages/demo/components/DemoFunctionComponent.tsx
@@ -1,5 +1,24 @@
 import { useEffect } from "react";
 
+type ProductProps = {
+    productName: string;
+    productPrice: number;
+};
+
+const DemoProductDetail = ({ productName, productPrice }: ProductProps) => {
+    useEffect(() => {
+        console.log(`Rendering ProductDetail for: ${productName}`);
+    }, [productName, productPrice]); // Log when productName or productPrice changes
+
+    return (
+        <div>
+            <h2>{productName}</h2>
+            <p>Price: ${productPrice.toFixed(2)}</p>
+        </div >
+    );
+};
+
+
 type IProps = {
     message: string;
 }
@@ -12,7 +31,8 @@ const DemoFunctionComponent = ({ message }: IProps) => {
 
     return (
         <div>
-            <p>{message}</p>
+            <h1>{message}</h1>
+            <DemoProductDetail productName="Awesome T-Shirt" productPrice={19.99} />
         </div>
     );
 };
