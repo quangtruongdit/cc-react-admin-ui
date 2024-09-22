71af99b70235cdb2bb2087b3f4b13d7877b635bb
QuangTruong <quangtruond.it@gmail.com>
2024-09-22 16:36:42 +0700

    Demo lazy load suspend

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,8 +1,13 @@
-import BarChartBox from "../../components/barChartBox/BarChartBox";
+import React, { Suspense } from "react";
+// import BarChartBox from "../../components/barChartBox/BarChartBox";
 import BigChartBox from "../../components/bigChartBox/BigChartBox";
-import ChartBox from "../../components/chartBox/ChartBox";
+// import ChartBox from "../../components/chartBox/ChartBox";
 import PieChartBox from "../../components/pieCartBox/PieChartBox";
 import TopBox from "../../components/topBox/TopBox";
+
+const ChartBox = React.lazy(() => import('../../components/chartBox/ChartBox'));
+const BarChartBox = React.lazy(() => import('../../components/barChartBox/BarChartBox'));
+
 import {
   barChartBoxRevenue,
   barChartBoxVisit,
@@ -19,32 +24,65 @@ const Home = () => {
       <div className="box box1">
         <TopBox />
       </div>
-      <div className="box box2">
-        <ChartBox {...chartBoxUser} />
-      </div>
-      <div className="box box3">
-        <ChartBox {...chartBoxProduct} />
-      </div>
-      <div className="box box4">
-        <PieChartBox />
-      </div>
-      <div className="box box5">
-        <ChartBox {...chartBoxConversion} />
-      </div>
-      <div className="box box6">
-        <ChartBox {...chartBoxRevenue} />
-      </div>
+      <Suspense fallback={<div>Lazy Loading User...</div>}>
+        <div className="box box2">
+          <LazyChartBoxUser />
+        </div>
+      </Suspense>
+      <Suspense fallback={<div>Loading...</div>}>
+        <div className="box box3">
+          <ChartBox {...chartBoxProduct} />
+        </div>
+      </Suspense>
+
+      <Suspense fallback={<div>Loading...</div>}>
+        <div className="box box4">
+          <PieChartBox />
+        </div>
+      </Suspense>
+      <Suspense fallback={<div>Loading...</div>}>
+        <div className="box box5">
+          <ChartBox {...chartBoxConversion} />
+        </div>
+      </Suspense>
+      <Suspense fallback={<div>Loading...</div>}>
+        <div className="box box6">
+          <ChartBox {...chartBoxRevenue} />
+        </div>
+      </Suspense>
       <div className="box box7">
         <BigChartBox />
       </div>
-      <div className="box box8">
-        <BarChartBox {...barChartBoxVisit} />
-      </div>
-      <div className="box box9">
-        <BarChartBox {...barChartBoxRevenue} />
-      </div>
+      <Suspense fallback={<div>Loading...</div>}>
+        <div className="box box8">
+          <BarChartBox {...barChartBoxVisit} />
+        </div>
+      </Suspense>
+
+      <Suspense fallback={<div>Loading...</div>}>
+        <div className="box box9">
+          <BarChartBox {...barChartBoxRevenue} />
+        </div>
+      </Suspense>
+
     </div>
   );
 };
+
+
+const fetchAboutData = (): Promise<string> => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("This data is fetched from the API.");
+    }, 2000); // Simulating a 2-second delay
+  });
+};
+
+const LazyChartBoxUser = React.lazy(async () => {
+  const data = await fetchAboutData(); // Simulate fetching data from API
+  return {
+    default: () => <div><ChartBox {...chartBoxUser} /></div>,
+  };
+});
 
 export default Home;
